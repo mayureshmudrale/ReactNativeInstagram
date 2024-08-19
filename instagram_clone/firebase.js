c5f409b5e66f774c7a8412d2c33f31cee4effcad
mayureshmudrale <71888845+mayureshmudrale@users.noreply.github.com>
2024-08-19 15:59:03 +0530

    Update firebase.js

--- instagram_clone/firebase.js
+++ instagram_clone/firebase.js
@@ -1,13 +1,13 @@
 import firebase from '@react-native-firebase/app'
 const firebaseConfig = {
-    apiKey: "AIzaSyB2BLhFq70RhRMYtWEZSs-K91HIzWL81P8",
-    authDomain: "instagram-clone-44b7d.firebaseapp.com",
-    databaseURL: "https://instagram-clone-44b7d.firebaseio.com",
-    projectId: "instagram-clone-44b7d",
-    storageBucket: "instagram-clone-44b7d.appspot.com",
-    messagingSenderId: "382292508238",
-    appId: "1:382292508238:web:988ebedf277b0800dc4d1f",
-    measurementId: "G-L5XDPES7GX"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1",
+    measurementId: ""
   };
   
   const firebaseApp = firebase.initializeApp(firebaseConfig);
